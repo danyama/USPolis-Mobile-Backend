bd7594af4d4f399db37c99a673b67572c430e54e
Rodrigo Magaldi <rod.magaldi@gmail.com>
2023-01-11 21:15:40 -0300

    add db connection setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const app: Express = express();
+app.use(express.json());
+
 const port = 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Server running");
+const prisma = new PrismaClient();
+
+app.get("/", async (req: Request, res: Response) => {
+  await prisma.$connect();
+  const allClassrooms = await prisma.classroom.findMany();
+
+  res.json(allClassrooms);
 });
 
 app.listen(port, () => {
