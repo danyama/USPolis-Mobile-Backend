88a44d90f7c5c3e7debcdfa918d427a36533a8ae
Rodrigo Magaldi <rod.magaldi@gmail.com>
2023-01-15 18:12:02 -0300

    add class details

--- src/routes/classes-routes.ts
+++ src/routes/classes-routes.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import { Router, Request, Response } from "express";
 import { Event } from "../types";
-import { getAbbreviatedClassCode } from "../utils";
+import { getAbbreviatedClassCode, mapWeekDays } from "../utils";
 
 export const classesRoute = Router();
 
@@ -20,4 +20,47 @@ classesRoute.get("", async (req: Request, res: Response) => {
   }));
 
   res.json(distinctClasses);
+});
+
+classesRoute.get("/detail", async (req: Request, res: Response) => {
+  const { subjectCode, classCode } = req.query;
+
+  const fullClassCode = (
+    classCode?.length === 1 ? `0${classCode}` : classCode
+  ) as string;
+
+  const classInfo = await prisma.events.findMany({
+    where: {
+      subject_code: {
+        equals: subjectCode as string,
+      },
+      class_code: {
+        endsWith: fullClassCode,
+      },
+    },
+  });
+
+  if (classInfo.length === 0) {
+    res.status(404);
+    res.json({ detail: "Turma não encontrada!" });
+    return;
+  }
+
+  const classDetail = {
+    subjectName: classInfo?.[0]?.subject_name,
+    subjectCode: classInfo?.[0]?.subject_code,
+    classCode: classInfo?.[0]?.class_code,
+    building: classInfo?.[0]?.building,
+    professor: classInfo?.[0]?.professor,
+    startPeriod: classInfo?.[0]?.start_period,
+    endPeriod: classInfo?.[0]?.end_period,
+    schedule: classInfo.map((event: Event) => ({
+      weekDay: mapWeekDays(event?.week_day),
+      startTime: event?.start_time,
+      endTime: event?.end_time,
+      classroom: event?.classroom,
+    })),
+  };
+
+  res.json(classDetail);
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,19 @@
-export const getAbbreviatedClassCode = (classCode: string) => {
+export const getAbbreviatedClassCode = (classCode: string): string => {
   const abbreviatedClassCode = classCode.slice(-2);
 
   if (abbreviatedClassCode[0] == "0") {
     return abbreviatedClassCode[1];
   }
   return abbreviatedClassCode;
+};
+
+export const mapWeekDays = (weekDay: string): string | undefined => {
+  const map = new Map<string, string>();
+  map.set("seg", "Segunda-feira");
+  map.set("ter", "Terça-feira");
+  map.set("qua", "Quarta-feira");
+  map.set("qui", "Quinta-feira");
+  map.set("sex", "Sexta-feira");
+
+  return map.get(weekDay);
 };
