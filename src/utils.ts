a017da749abb311c20488c729d1a7a880bf81341
Rodrigo Magaldi <rod.magaldi@gmail.com>
2023-03-23 13:54:38 -0300

    add prod

--- src/utils.ts
+++ src/utils.ts
@@ -36,7 +36,7 @@ const findFloor = async (prisma: PrismaClient, classroom_name: string, building:
 
 export const mapResults = async (prisma: PrismaClient, results: ListedEvent[]) => {
   return Promise.all(results
-    .filter((element) => element._id.created_by === "amelia" || element._id.created_by === "eletrica-manual")
+    .filter((element) => element._id.created_by === "amelia" || element._id.created_by === "eletrica-manual" || element._id.created_by === "producao-manual")
     .sort((a, b) => a._id.subject_code.localeCompare(b._id.subject_code) || a._id.class_code.localeCompare(b._id.class_code))
     .map(async (result: ListedEvent) => ({
     ...result._id,
